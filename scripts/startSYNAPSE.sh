e14544deac186ebdfd6e6a66cb0e8f03a9a1504b
eneagizzarelli <s314887@studenti.polito.it>
2024-04-12 16:48:45 +0200

    fix: modified start script

--- scripts/startSYNAPSE.sh
+++ scripts/startSYNAPSE.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+chmod +x /home/user/SYNAPSE/src/SYNAPSE.py
+chmod +x /home/user/SYNAPSE/src/initializations.py
+chmod +x /home/user/SYNAPSE/src/ai_requests.py
+
 python3 /home/user/SYNAPSE/src/SYNAPSE.py
