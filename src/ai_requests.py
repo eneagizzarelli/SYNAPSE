d8850b184aeb2598319a7ab461bcc3e308a84ec8
eneagizzarelli <s314887@studenti.polito.it>
2024-04-12 15:41:10 +0200

    fix: reached common point with shelLM

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -8,4 +8,7 @@ model = "gpt-3.5-turbo-16k"
 
 def generate_response(messages):
     response = openai.chat.completions.create(model=model, messages = messages, temperature = 0.0, max_tokens = 800)
-    return response
+
+    terminal_message = {"role": 'assistant', "content": response.choices[0].message.content}
+    
+    return terminal_message
