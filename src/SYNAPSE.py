3c7f31c0ea6fbeece151020bb57e72ad7e2e885e
eneagizzarelli <s314887@studenti.polito.it>
2024-04-15 15:12:47 +0200

    fix: fixed mysql service exit

--- src/SYNAPSE.py
+++ src/SYNAPSE.py
@@ -16,8 +16,7 @@ def main():
                     mysql_messages = mysql_simulation(mysql_messages)
                 except KeyboardInterrupt:
                     mysql_messages.append({"role": "user", "content": "\n"})
-                    print("")
-                    raise KeyboardInterrupt
+                    terminal_messages.append({"role": "user", "content": "Coming back from mysql server. \n"})
             else:
                 try:
                     terminal_messages = terminal_simulation(terminal_messages)
