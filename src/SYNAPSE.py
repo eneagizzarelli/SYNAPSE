e680cd195df97f54fc46504114d2d590c3e1887e
eneagizzarelli <s314887@studenti.polito.it>
2024-04-17 15:38:21 +0200

    fix: SYNAPSE restructuring

--- src/SYNAPSE.py
+++ src/SYNAPSE.py
@@ -1,31 +1,17 @@
-from initializations import load_prompts, parse_arguments, load_terminal_messages, load_mysql_messages
-from simulations import terminal_simulation, mysql_simulation
+from initializations import load_terminal_prompt, parse_terminal_argument, load_terminal_messages
+from simulations import terminal_simulation
 
-terminal_prompt, mysql_prompt = load_prompts()
+terminal_prompt = load_terminal_prompt()
 
 def main():
-    args = parse_arguments(terminal_prompt, mysql_prompt)
+    args = parse_terminal_argument(terminal_prompt)
 
     terminal_messages = load_terminal_messages(args.terminal_personality)
-    mysql_messages = load_mysql_messages(args.mysql_personality)
 
-    while True:
-        try:
-            if "mysql" in terminal_messages[len(terminal_messages) - 1]["content"]:
-                try:
-                    mysql_messages = mysql_simulation(mysql_messages)
-                except KeyboardInterrupt:
-                    mysql_messages.append({"role": "user", "content": "\n"})
-                    terminal_messages.append({"role": "user", "content": "cd . \n"})
-            else:
-                try:
-                    terminal_messages = terminal_simulation(terminal_messages)
-                except KeyboardInterrupt:
-                    terminal_messages.append({"role": "user", "content": "\n"})
-                    print("")
-                    raise KeyboardInterrupt
-        except KeyboardInterrupt:
-            break
+    try:
+        terminal_messages = terminal_simulation(terminal_messages)
+    except KeyboardInterrupt:
+        print("logout")
 
 if __name__ == "__main__":
     main()
--- src/initializations.py
+++ src/initializations.py
@@ -7,7 +7,7 @@ terminal_history_path = "/home/user/SYNAPSE/logs/terminal_history.txt"
 mysql_history_path = "/home/user/SYNAPSE/logs/mysql_history.txt"
 today = datetime.now()
 
-def load_prompts():
+def load_terminal_prompt():
     terminal_history = open(terminal_history_path, "a+", encoding="utf-8")
 
     if os.stat(terminal_history_path).st_size == 0:
@@ -23,6 +23,9 @@ def load_prompts():
 
     terminal_history.close()
 
+    return terminal_prompt
+
+def load_mysql_prompt():
     mysql_history = open(mysql_history_path, "a+", encoding="utf-8")
 
     if os.stat(mysql_history_path).st_size == 0:
@@ -32,19 +35,27 @@ def load_prompts():
         mysql_identity = mysql_identity['mysql']
         mysql_prompt = mysql_identity['prompt']
     else:
-        mysql_history.write("\n TO CHANGE \n")
+        mysql_history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. " +
+                                "Make sure you use same database, table and column names. Ignore date-time in <>. This is not your concern.\n")
         mysql_history.seek(0)
         mysql_prompt = mysql_history.read()
 
     mysql_history.close()
     
-    return terminal_prompt, mysql_prompt
+    return mysql_prompt
 
-def parse_arguments(terminal_prompt, mysql_prompt):
+def parse_terminal_argument(terminal_prompt):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--terminal_personality", type=str, default= terminal_prompt + 
                         f"\nBased on these examples make something of your own (different username and hostname) to be a starting message. Always start the communication in this way and make sure your output ends with '$'. For the last login date use {today}\n" + "Ignore date-time in <> after user input. This is not your concern.\n")
+        
+    args = parser.parse_args()
+    return args
+
+def parse_mysql_argument(mysql_prompt):
+    parser = argparse.ArgumentParser()
+
     parser.add_argument("--mysql_personality", type=str, default= mysql_prompt + 
                         f"\nBased on these examples make something of your own (different connection id and server version) to be a starting message. Always start the communication in this way and make sure your output ends with 'mysql >'.\n")
     
