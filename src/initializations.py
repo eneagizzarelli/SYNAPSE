d6eca93513c2a8abb9a41e171ab0d857dce913d2
eneagizzarelli <s314887@studenti.polito.it>
2024-04-17 15:38:21 +0200

    fix: restructuring SYNAPSE code

--- src/initializations.py
+++ src/initializations.py
@@ -7,22 +7,26 @@ terminal_history_path = "/home/user/SYNAPSE/logs/terminal_history.txt"
 mysql_history_path = "/home/user/SYNAPSE/logs/mysql_history.txt"
 today = datetime.now()
 
-def load_prompts():
-    terminal_history = open(terminal_history_path, "a+", encoding="utf-8")
-
+def load_terminal_prompt():
     if os.stat(terminal_history_path).st_size == 0:
         with open("/home/user/SYNAPSE/terminal_personality.yml", 'r', encoding="utf-8") as personality_file:
             terminal_identity = yaml.safe_load(personality_file)
         terminal_identity = terminal_identity['personality']
         terminal_prompt = terminal_identity['prompt']
+
+        return terminal_prompt
     else:
-        terminal_history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. " +
-                                "Make sure you use same file and folder names. Ignore date-time in <>. This is not your concern.\n")
+        terminal_history = open(terminal_history_path, "a+", encoding="utf-8")
+
+        terminal_history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. Make sure you use same file and folder names. Ignore date-time in <>. This is not your concern.\n")
         terminal_history.seek(0)
         terminal_prompt = terminal_history.read()
 
-    terminal_history.close()
+        terminal_history.close()
 
+    return terminal_prompt
+
+def load_mysql_prompt():
     mysql_history = open(mysql_history_path, "a+", encoding="utf-8")
 
     if os.stat(mysql_history_path).st_size == 0:
@@ -32,19 +36,26 @@ def load_prompts():
         mysql_identity = mysql_identity['mysql']
         mysql_prompt = mysql_identity['prompt']
     else:
-        mysql_history.write("\n TO CHANGE \n")
+        mysql_history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. Make sure you use same database, table and column names. Ignore date-time in <>. This is not your concern.\n")
         mysql_history.seek(0)
         mysql_prompt = mysql_history.read()
 
     mysql_history.close()
     
-    return terminal_prompt, mysql_prompt
+    return mysql_prompt
 
-def parse_arguments(terminal_prompt, mysql_prompt):
+def parse_terminal_argument(terminal_prompt):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--terminal_personality", type=str, default= terminal_prompt + 
                         f"\nBased on these examples make something of your own (different username and hostname) to be a starting message. Always start the communication in this way and make sure your output ends with '$'. For the last login date use {today}\n" + "Ignore date-time in <> after user input. This is not your concern.\n")
+        
+    args = parser.parse_args()
+    return args
+
+def parse_mysql_argument(mysql_prompt):
+    parser = argparse.ArgumentParser()
+
     parser.add_argument("--mysql_personality", type=str, default= mysql_prompt + 
                         f"\nBased on these examples make something of your own (different connection id and server version) to be a starting message. Always start the communication in this way and make sure your output ends with 'mysql >'.\n")
     
