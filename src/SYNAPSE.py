911a2ed0c45c73f1a32a1999cdf248a19c8883f6
eneagizzarelli <s314887@studenti.polito.it>
2024-04-18 10:14:40 +0200

    fix: minor fixes

--- src/SYNAPSE.py
+++ src/SYNAPSE.py
@@ -1,9 +1,9 @@
 from initializations import load_terminal_prompt, parse_terminal_argument, load_terminal_messages
 from simulations import terminal_simulation
 
-terminal_prompt = load_terminal_prompt()
-
 def main():
+    terminal_prompt = load_terminal_prompt()
+    
     args = parse_terminal_argument(terminal_prompt)
 
     terminal_messages = load_terminal_messages(args.terminal_personality)
