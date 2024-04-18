c36dd163d934758fc407674cb83fe5e3d8d50978
eneagizzarelli <s314887@studenti.polito.it>
2024-04-18 16:31:04 +0200

    fix: minor fixes

--- src/SYNAPSE.py
+++ src/SYNAPSE.py
@@ -1,20 +1,17 @@
 from initializations import load_terminal_prompt, parse_terminal_argument, load_terminal_messages
 from simulations import terminal_simulation
 
-terminal_history_path = "/home/user/SYNAPSE/logs/terminal_history.txt"
 
-terminal_history = open(terminal_history_path, "a+", encoding="utf-8")
-
-terminal_prompt = load_terminal_prompt(terminal_history)
+terminal_prompt = load_terminal_prompt()
 
 
 def main():
     args = parse_terminal_argument(terminal_prompt)
 
-    terminal_messages = load_terminal_messages(args.terminal_personality, terminal_history)
+    terminal_messages = load_terminal_messages(args.terminal_personality)
 
     try:
-        terminal_simulation(terminal_messages, terminal_history)
+        terminal_simulation(terminal_messages)
     except KeyboardInterrupt:
         print("logout")
 
