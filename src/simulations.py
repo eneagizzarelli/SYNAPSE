02dc625ef042047efe8b0d6e5538f189f4e0d206
eneagizzarelli <s314887@studenti.polito.it>
2024-04-19 12:48:17 +0200

    fix: introduced mysql service

--- src/simulations.py
+++ src/simulations.py
@@ -60,10 +60,10 @@ def terminal_simulation(terminal_messages):
                 print("Bye")
                 
                 terminal_messages.append({"role": "user", "content": "\n" + f"\t<{datetime.now()}>\n"})
-                terminal_history.write(" " + f"\t<{datetime.now()}>\n")
+                terminal_history.write("\n" + f"\t<{datetime.now()}>\n")
             else:
                 terminal_messages.append({"role": "user", "content": user_input + f"\t<{datetime.now()}>\n" })
-                terminal_history.write("\n" + user_input + f"\t<{datetime.now()}>\n")
+                terminal_history.write(" " + user_input + f"\t<{datetime.now()}>\n")
         else:
             user_input = input(f'\n{terminal_messages[len(terminal_messages) - 1]["content"]}'.strip() + " ")
             # check over user trying to exit
