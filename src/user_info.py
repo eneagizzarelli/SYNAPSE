e5a157a1e958d57158e76bd9eb656c654771eda7
eneagizzarelli <s314887@studenti.polito.it>
2024-04-22 11:55:28 +0200

    fix: minor fixes

--- src/user_info.py
+++ src/user_info.py
@@ -10,7 +10,7 @@ def get_ssh_client_ip():
         client_ip = ssh_connection_info.split()[0]
         
         if not os.path.exists(base_path + "logs/" + client_ip):
-            os.makedirs(base_path + client_ip)
+            os.makedirs(base_path + "logs/" + client_ip)
 
         return client_ip
 
