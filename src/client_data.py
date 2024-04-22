9e3a896b05b8f6ee5fa3bd55f184e2b23f201fcd
eneagizzarelli <s314887@studenti.polito.it>
2024-04-22 13:15:12 +0200

    feat: added geolocation

--- src/client_data.py
+++ src/client_data.py
@@ -1,5 +1,6 @@
 import os
 import json
+import geoip2.database
 
 base_path = "/home/user/SYNAPSE/"
 
@@ -23,6 +24,7 @@ def initialize_client_data(client_ip, client_port, server_port):
         "ip": client_ip,
         "client_port": client_port,
         "server_port": server_port,
+        "geolocation": None,
         "number_of_connections": 0,
         "session_durations_in_seconds": []
     }
@@ -30,6 +32,46 @@ def initialize_client_data(client_ip, client_port, server_port):
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
         json.dump(data, client_data_file)
         client_data_file.write("\n")
+
+def write_client_geolocation(client_ip):
+    database_path = base_path + "data/" + "GeoLite2-City.mmdb"
+    reader = geoip2.database.Reader(database_path)
+
+    try:
+        response = reader.city(client_ip)
+        
+        geolocation_data = {
+            "country": {
+                "name": response.country.name,
+                "iso_code": response.country.iso_code
+            },
+            "region": response.subdivisions.most_specific.name,
+            "city": response.city.name,
+            "postal_code": response.postal.code,
+            "location": {
+                "latitude": response.location.latitude,
+                "longitude": response.location.longitude,
+                "time_zone": response.location.time_zone,
+                "accuracy_radius": response.location.accuracy_radius
+            },
+            "continent": {
+                "name": response.continent.name,
+                "code": response.continent.code
+            }
+        }
+        
+        with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "r") as client_data_file:
+            data = json.load(client_data_file)
+            
+        data["geolocation"] = geolocation_data
+
+        with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
+            json.dump(data, client_data_file)
+            client_data_file.write("\n")
+    except geoip2.errors.AddressNotFoundError:
+        return None
+    finally:
+        reader.close()
 
 def increment_client_number_of_connections(client_ip):
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "r") as client_data_file:
