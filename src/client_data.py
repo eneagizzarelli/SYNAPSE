c3f9a722e8376bba81403519268985675f4a6246
eneagizzarelli <s314887@studenti.polito.it>
2024-04-22 15:28:02 +0200

    fix: restructured user data

--- src/client_data.py
+++ src/client_data.py
@@ -4,43 +4,28 @@ import geoip2.database
 
 base_path = "/home/user/SYNAPSE/"
 
-def get_client_ip():
-    ssh_connection_info = os.environ.get("SSH_CONNECTION")
-    
-    if ssh_connection_info:
-        client_ip = ssh_connection_info.split()[0]
-        client_port = ssh_connection_info.split()[1]
-        server_port = ssh_connection_info.split()[3]
-        
-        if not os.path.exists(base_path + "logs/" + client_ip):
-            os.makedirs(base_path + "logs/" + client_ip)
-
-            initialize_client_data(client_ip, client_port, server_port)
-
-        return client_ip
-
-def initialize_client_data(client_ip, client_port, server_port):
+def initialize_client_data(client_ip, client_port, server_port, client_geolocation):
     data = {
         "ip": client_ip,
         "client_port": client_port,
         "server_port": server_port,
-        "geolocation": None,
+        "geolocation": client_geolocation,
         "number_of_connections": 0,
         "session_durations_in_seconds": []
     }
 
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
-        json.dump(data, client_data_file)
+        json.dump(data, client_data_file, indent=4)
         client_data_file.write("\n")
 
-def write_client_geolocation(client_ip):
+def get_client_geolocation(client_ip):
     database_path = base_path + "data/" + "GeoLite2-City.mmdb"
     reader = geoip2.database.Reader(database_path)
 
     try:
         response = reader.city(client_ip)
         
-        geolocation_data = {
+        client_geolocation = {
             "country": {
                 "name": response.country.name,
                 "iso_code": response.country.iso_code
@@ -59,19 +44,28 @@ def write_client_geolocation(client_ip):
                 "code": response.continent.code
             }
         }
-        
-        with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "r") as client_data_file:
-            data = json.load(client_data_file)
-            
-        data["geolocation"] = geolocation_data
 
-        with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
-            json.dump(data, client_data_file)
-            client_data_file.write("\n")
+        return client_geolocation
     except geoip2.errors.AddressNotFoundError:
         return None
     finally:
         reader.close()
+
+def get_client_ip():
+    ssh_connection_info = os.environ.get("SSH_CLIENT")
+    
+    if ssh_connection_info:
+        client_ip = ssh_connection_info.split()[0]
+        client_port = ssh_connection_info.split()[1]
+        server_port = ssh_connection_info.split()[2]
+        
+        if not os.path.exists(base_path + "logs/" + client_ip):
+            os.makedirs(base_path + "logs/" + client_ip)
+
+            client_geolocation = get_client_geolocation(client_ip)
+            initialize_client_data(client_ip, client_port, server_port, client_geolocation)
+
+        return client_ip
 
 def increment_client_number_of_connections(client_ip):
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "r") as client_data_file:
@@ -80,7 +74,7 @@ def increment_client_number_of_connections(client_ip):
     data["number_of_connections"] += 1
 
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
-        json.dump(data, client_data_file)
+        json.dump(data, client_data_file, indent=4)
         client_data_file.write("\n")
 
 def write_client_session_duration_in_seconds(session_duration_in_seconds, client_ip):
@@ -90,5 +84,5 @@ def write_client_session_duration_in_seconds(session_duration_in_seconds, client
     data["session_durations_in_seconds"].append(session_duration_in_seconds)
 
     with open(base_path + "logs/" + client_ip + "/" + client_ip + "_data.json", "w") as client_data_file:
-        json.dump(data, client_data_file)
+        json.dump(data, client_data_file, indent=4)
         client_data_file.write("\n")
