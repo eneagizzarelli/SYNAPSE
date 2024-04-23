4aa263720353b78e8304a202e284c5e21332000b
eneagizzarelli <s314887@studenti.polito.it>
2024-04-23 16:09:12 +0200

    feat: added tab completion

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -7,13 +7,6 @@ openai.api_key = config["OPENAI_API_KEY"]
 
 model = "gpt-3.5-turbo-0125"
 
-def generate_response(messages):
-    response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.0, max_tokens = 800)
-    terminal_msg = response.choices[0].message.content
-    terminal_message = {"role": 'assistant', "content": terminal_msg}
-    
-    return terminal_message
-
 def generate_tab_completions(text):
     response = openai.chat.completions.create(model = model, messages = text, max_tokens = 5, n = 5)
     completions = [choice['text'].strip() for choice in response['choices']]
@@ -30,3 +23,10 @@ def completer(text, state):
     
 readline.set_completer(completer)
 readline.parse_and_bind('tab: complete')
+
+def generate_response(messages):
+    response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.0, max_tokens = 800)
+    terminal_msg = response.choices[0].message.content
+    terminal_message = {"role": 'assistant', "content": terminal_msg}
+    
+    return terminal_message
