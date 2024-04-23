3e945af7de399808a7ded7c63ef574ba1af15619
eneagizzarelli <s314887@studenti.polito.it>
2024-04-23 16:33:11 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -8,13 +8,13 @@ openai.api_key = config["OPENAI_API_KEY"]
 model = "gpt-3.5-turbo-0125"
 
 def generate_tab_completions(text):
-    response = openai.chat.completions.create(model = model, messages = text, max_tokens = 5, n = 5)
-    completions = [choice['text'].strip() for choice in response['choices']]
+    response = openai.chat.completions.create(model = model, messages = text, temperature = 0.0, max_tokens = 5, n = 5)
+    completions = response.choices
     return completions
 
 def completer(text, state):
-    print(text)
     completions = generate_tab_completions(text)
+    print(completions)
     matches = [option for option in completions if option.startswith(text)]
     if state < len(matches):
         return matches[state]
