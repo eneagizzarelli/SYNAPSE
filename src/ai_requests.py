266d6bb3f17bb32e436d01ebedbe7afb8107c111
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 17:33:36 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -5,9 +5,11 @@ import readline
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-def generate_tab_completions(prompt):
-    response = openai.completions.create(model = "gpt-3.5-turbo-instruct", prompt = prompt, temperature = 0.1, max_tokens = 50)
-    completions = response.choices[0].text.split("\n")
+model = "gpt-3.5-turbo-0125"
+
+def generate_tab_completions(messages):
+    response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.1, max_tokens = 50)
+    completions = response.choices[0].message.content.split("\n")
 
     return completions
 
@@ -15,13 +17,17 @@ def completer(text, state):
     if text == "":
         return None
     
-    prompt = "Emulate the tab autocompletion of a Linux terminal. Generate many DIFFERENT words separated by '\n' to complete the already started one. If you don't know what to answer, do not print anything. Do not start in any case a conversation with the user. A terminal would not do so. Start from the following text and complete it: " + text
+    messages = [{"role": 'system', "content": "Emulate the tab autocompletion of a Linux terminal. " + 
+                 "Generate many DIFFERENT words separated by '\n' to complete the already started one. " + 
+                 "If you don't know what to answer, do not print anything. " + 
+                 "Do not start in any case a conversation with the user. A terminal would not do so. " + 
+                 "Start from the following text and complete it. \n"}]
+    messages.append({"role": 'user', "content": text})
 
-    completions = generate_tab_completions(prompt)
+    completions = generate_tab_completions(messages)
 
     matches = [option for option in completions if option.startswith(text) and option != '']
 
-    print(state)
     print(matches)
     print(matches[state])
     print(len(matches))
@@ -35,7 +41,7 @@ readline.set_completer(completer)
 readline.parse_and_bind('tab: complete')
 
 def generate_response(messages):
-    response = openai.chat.completions.create(model = "gpt-3.5-turbo-0125", messages = messages, temperature = 0.0, max_tokens = 800)
+    response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.0, max_tokens = 800)
     msg = response.choices[0].message.content
     message = {"role": 'assistant', "content": msg}
     
