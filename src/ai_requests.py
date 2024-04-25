df46cc8f9d9ca2f08e89c59de44801399f533e12
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 17:14:54 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -5,8 +5,8 @@ import readline
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-def generate_tab_completions(messages):
-    response = openai.chat.completions.create(model = "gpt-3.5-turbo-instruct", messages = messages, temperature = 0.1, max_tokens = 50)
+def generate_tab_completions(prompt):
+    response = openai.completions.create(model = "gpt-3.5-turbo-instruct", prompt = prompt, temperature = 0.1, max_tokens = 50)
 
     print(response.choices[0])
     completions = response.choices[0].message.content.split("\n")
@@ -17,13 +17,13 @@ def completer(text, state):
     if text == "":
         return None
     
-    messages = [{"role": 'system', "content": "Emulate the tab autocompletion of a Linux terminal. " + 
-                 "Generate many words separated by '\n' to complete the already started one. " + 
-                 "If you don't know what to answer, do not print anything. " +
-                 "Do not start in any case a conversation with the user. A terminal would not do so." +
-                 "Start from the text that follows. "}, {"role": 'user', "content": text}]
+    prompt = "Emulate the tab autocompletion of a Linux terminal. " 
+    + "Generate many words separated by '\n' to complete the already started one. " 
+    + "If you don't know what to answer, do not print anything. " 
+    + "Do not start in any case a conversation with the user. A terminal would not do so." 
+    + "Start from the text that follows. \n" + text
 
-    completions = generate_tab_completions(messages)
+    completions = generate_tab_completions(prompt)
 
     print(completions)
 
