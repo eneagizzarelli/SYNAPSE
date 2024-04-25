7627a51187629ced88932819fdb0af556c28767f
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 17:09:09 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -26,6 +26,7 @@ def completer(text, state):
     matches = [option for option in completions if option.startswith(text)]
 
     if state < len(matches):
+        print(matches[state])
         return matches[state]
     else:
         return None
