0f1fb8a7b4b7a045140becfc197d8acf0a6a7493
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 22:26:23 +0200

    fix: minor fixes

--- src/SYNAPSE.py
+++ src/SYNAPSE.py
@@ -4,9 +4,10 @@ from initializations import load_terminal_prompt, parse_terminal_argument, load_
 from simulations import terminal_simulation
 from client_data import get_client_ip, increment_client_number_of_connections, write_client_session_duration_in_seconds
 
+client_ip = get_client_ip()
+
 def main():
-    client_ip = get_client_ip()
-    increment_client_number_of_connections(client_ip)
+    increment_client_number_of_connections()
     
     terminal_prompt = load_terminal_prompt(client_ip)
     args = parse_terminal_argument(terminal_prompt, client_ip)
