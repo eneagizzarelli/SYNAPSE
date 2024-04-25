0b40b4b56da7a5d54430f8d85cd871f1015b0306
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 18:28:22 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -9,35 +9,30 @@ model = "gpt-3.5-turbo-0125"
 
 def generate_tab_completions(messages):
     response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.1, max_tokens = 10)
-    completions = [choice.message.content for choice in response.choices]
-
-    print(completions)
+    completions = response.choices[0].message.content
 
     return completions
 
-def complete(text, state):
-
-    if state == 0:
-        if text == "":
-            return None
-    
-        messages = [{"role": 'system', "content": "Emulate the tab autocompletion of a Linux terminal. " + 
-                 "Generate words to complete the already started one. " + 
-                 "If you don't know what to answer, do not print anything. " + 
-                 "Do not start in any case a conversation with the user. A terminal would not do so. " + 
-                 "Start from the following text and complete it. \n"}]
-        messages.append({"role": 'user', "content": text})
-
-        completions = generate_tab_completions(messages)
-        matches = [option for option in completions if option.startswith(text)]
-
-    if state < len(matches):
-        return matches[state]
-    else:
+def completer(text, state):
+    if text == "":
         return None
 
+    messages = [{"role": 'system', "content": "Emulate the tab autocompletion of a Linux terminal. " + 
+                "Generate words to complete the already started one. " + 
+                "If you don't know what to answer, do not print anything. " + 
+                "Do not start in any case a conversation with the user. A terminal would not do so. " + 
+                "Start from the following text and complete it. \n"}]
+    messages.append({"role": 'user', "content": text})
+
+    completions = generate_tab_completions(messages)
     
-readline.set_completer(complete)
+    if len(completions.split(" ")) > 1:
+        print("\n" + completions)
+    else:
+        return completions
+
+    
+readline.set_completer(completer)
 readline.parse_and_bind('tab: complete')
 
 def generate_response(messages):
