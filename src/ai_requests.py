487ed67ce506179b1e598862b6bba3c189a7fd80
eneagizzarelli <s314887@studenti.polito.it>
2024-04-25 16:20:56 +0200

    fix: minor fixes

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -9,9 +9,8 @@ model = "gpt-3.5-turbo-0125"
 
 def generate_tab_completions(messages):
     response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.0, max_tokens = 10)
+    print(response.choices)
     completions = [choice.message.content for choice in response.choices]
-
-    print("COMPLETIONS: " + completions)
 
     return completions
 
