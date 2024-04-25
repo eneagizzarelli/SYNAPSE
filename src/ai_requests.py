5ede1321d064755cde36e14758c9b0d59ce6ff6d
eneagizzarelli <s314887@studenti.polito.it>
2024-04-26 00:37:12 +0200

    feat: implementing ls understanding from completion

--- src/ai_requests.py
+++ src/ai_requests.py
@@ -9,6 +9,8 @@ openai.api_key = config["OPENAI_API_KEY"]
 
 model = "gpt-3.5-turbo-0125"
 
+base_path = "/home/user/SYNAPSE/logs"
+
 def generate_tab_completions(messages):
     response = openai.chat.completions.create(model = model, messages = messages, temperature = 0.0, max_tokens = 5)
     completions = response.choices[0].message.content
@@ -20,20 +22,25 @@ def completer(text, state):
         if text == "":
             return None
 
-        
+        tab_completion_history = open(base_path + "tab_completion_history.txt", "a+", encoding="utf-8")
+        tab_completion_history.seek(0)
+        tab_completion_history_content = tab_completion_history.read()
 
         messages = [{"role": 'system', "content": "Emulate the tab autocompletion of a Linux terminal. " + 
                     "Generate words separated by " " to complete the already started one. " + 
                     "If you don't know what to answer, do not print anything. " + 
-                    "Do not start in any case a conversation with the user. A terminal would not do so. " + 
+                    "Do not start in any case a conversation with the user. A terminal would not do so. \n" + 
+                    "To generate completions, consider also the following text: \n" + tab_completion_history_content + "\n" +
                     "Start from the following text and complete it. \n"}]
         messages.append({"role": 'user', "content": text})
 
         completions = generate_tab_completions(messages)
         
         if len(completions.split(" ")) > 1:
+            tab_completion_history.close()
             return None
         else:
+            tab_completion_history.close()
             return completions
 
     
