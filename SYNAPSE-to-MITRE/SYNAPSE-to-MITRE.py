9900dc643c3ef84d1a6fe94950d0c7fa7eceb4be
eneagizzarelli <s314887@studenti.polito.it>
2024-05-08 12:37:11 +0200

    fix: added console output to SYNASE-to-MITRE

--- SYNAPSE-to-MITRE/SYNAPSE-to-MITRE.py
+++ SYNAPSE-to-MITRE/SYNAPSE-to-MITRE.py
@@ -5,22 +5,35 @@ from classification import attack_happened, get_sentence, remove_classification_
 base_path = "/home/user/SYNAPSE/"
 
 def main():
+    print("Starting SYNAPSE-to-MITRE mapping...\n")
+
     for client_ip in os.listdir(base_path + "logs/"):
         if os.path.isdir(base_path + "logs/" + client_ip):
+
+            print("IP: " + client_ip + "\n")
+
             for classification_file in os.listdir(base_path + "logs/" + client_ip):
                 if os.path.isfile(base_path + "logs/" + client_ip + "/" + classification_file) and classification_file.startswith(client_ip + "_classification_history_"):
+
+                    print("- " + classification_file + ": ", end="")
+
                     if(attack_happened(classification_file, client_ip)):
+
+                        print("Attack happened.\n")
+
                         sentence = get_sentence(classification_file, client_ip)
 
-                        print(sentence)
+                        print("\t" + sentence + "\n")
 
                         classification = get_classification(sentence)
                         attack_object = get_attack_object(classification)
                         print_attack_object_to_file(attack_object, client_ip)
                     else :
-                        print("No attack happened.")
+                        print("No attack happened.\n")
 
                     remove_classification_history(classification_file, client_ip)
+
+    print("SYNAPSE-to-MITRE mapping finished.")
 
 if __name__ == "__main__":
     main()
