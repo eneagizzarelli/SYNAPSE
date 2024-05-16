47f8e2396d77c763cf938b963f6006b6144d48be
eneagizzarelli <s314887@studenti.polito.it>
2024-05-16 13:34:01 +0200

    fix: minor code fixes

--- src/client_data.py
+++ src/client_data.py
@@ -12,7 +12,7 @@ def get_client_ip():
 
 client_ip = get_client_ip()
 
-logs_ip_path = "/home/user/SYNAPSE/logs" + client_ip
+logs_ip_path = "/home/user/SYNAPSE/logs/" + client_ip
 logs_ip_data_path = logs_ip_path + "/" + client_ip + "_data.json"
 database_path = "/home/user/SYNAPSE/data/GeoLite2-City.mmdb"
 
--- src/simulations.py
+++ src/simulations.py
@@ -7,9 +7,9 @@ from client_data import client_ip, get_count_classification_history_files
 from initializations import load_mysql_prompt, parse_mysql_argument, load_mysql_messages
 from ai_requests import *
 
-logs_ip_terminal_history_path = "/home/user/SYNAPSE/logs" + client_ip + "/" + client_ip + "_terminal_history.txt"
-logs_ip_mysql_history_path = "/home/user/SYNAPSE/logs" + client_ip + "/" + client_ip + "_mysql_history.txt"
-logs_ip_classification_history_path = "/home/user/SYNAPSE/logs" + client_ip + "/" + client_ip + "_classification_history.txt"
+logs_ip_terminal_history_path = "/home/user/SYNAPSE/logs/" + client_ip + "/" + client_ip + "_terminal_history.txt"
+logs_ip_mysql_history_path = "/home/user/SYNAPSE/logs/" + client_ip + "/" + client_ip + "_mysql_history.txt"
+logs_ip_classification_history_path = "/home/user/SYNAPSE/logs/" + client_ip + "/" + client_ip + "_classification_history_"
 
 today = datetime.now()
 
@@ -22,7 +22,7 @@ def terminal_simulation(terminal_messages):
             raise KeyboardInterrupt
 
         if "mysql" in terminal_messages[len(terminal_messages) - 1]["content"].splitlines()[-1]:
-            run_mysql_simulation()
+            run_mysql_simulation(count_classification_history_files)
             print("\nBye")
             terminal_messages.append({"role": "user", "content": "cd ." + f"\t<{datetime.now()}>\n"})
 
@@ -80,7 +80,7 @@ def terminal_simulation(terminal_messages):
         terminal_history.close()
         classification_history.close()
 
-def run_mysql_simulation():
+def run_mysql_simulation(count_classification_history_files):
     mysql_prompt = load_mysql_prompt()
 
     args = parse_mysql_argument(mysql_prompt)
@@ -88,15 +88,13 @@ def run_mysql_simulation():
     mysql_messages = load_mysql_messages(args.mysql_personality)
 
     try:
-        mysql_simulation(mysql_messages)
+        mysql_simulation(mysql_messages, count_classification_history_files)
     except KeyboardInterrupt:
         pass
     except EOFError:
         pass
 
-def mysql_simulation(mysql_messages):
-    count_classification_history_files = get_count_classification_history_files()
-    
+def mysql_simulation(mysql_messages, count_classification_history_files):
     while True:
         if "exit" in mysql_messages[len(mysql_messages) - 1]["content"].splitlines()[-1] or "quit" in mysql_messages[len(mysql_messages) - 1]["content"].splitlines()[-1] or "\q" in mysql_messages[len(mysql_messages) - 1]["content"].splitlines()[-1]:
             break
