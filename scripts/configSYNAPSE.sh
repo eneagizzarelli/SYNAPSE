6d255b7b8a198cdca2d2f2d22933314f1b07245b
eneagizzarelli <s314887@studenti.polito.it>
2024-05-21 15:51:23 +0200

    fix: minor fixes

--- scripts/configSYNAPSE.sh
+++ scripts/configSYNAPSE.sh
@@ -11,7 +11,7 @@ git clone https://github.com/eneagizzarelli/SYNAPSE.git ${SYNAPSE_path}
 mkdir -p ${SYNAPSE_path}/logs
 mkdir -p ${SYNAPSE_path}/data
 
-curl -L -o ${SYNAPSE_path}/data/GeoLite2-City.mmdb https://git.io/GeoLite2-City.mmdb
+python ${SYNAPSE_path}/scripts/downloadGeoLiteDB.py
 
 sudo chown -R enea:enea ${SYNAPSE_path}
 sudo chmod -R 700 ${SYNAPSE_path}
