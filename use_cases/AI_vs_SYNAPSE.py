e563cb6db330fc60d6402d2d7448d31adcc6ce00
eneagizzarelli <s314887@studenti.polito.it>
2024-06-28 12:02:34 +0200

    feat: implementing AI_vs_SYNAPSE use case

--- use_cases/AI_vs_SYNAPSE.py
+++ use_cases/AI_vs_SYNAPSE.py
@@ -8,8 +8,9 @@ from ai_requests import generate_response
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-messages = [{"role": 'system', "content": "You are a normal Linux user interacting with a Linux OS terminal. + "
-                                        "Issue some commands to interact with the OS file system.\n"}]
+messages = [{"role": 'system', "content": "You are a normal Linux user interacting with a Linux OS terminal. " + 
+                                        "Issue some commands to interact with the OS file system. " + 
+                                        "Generate just the command you want to execute, nothing else. \n"}]
 
 while True:
     try:
@@ -21,10 +22,14 @@ while True:
 
         messages.append({"role": 'user', "content": SYNAPSE_output})
 
+        print(messages)
+
         AI_input = generate_response(messages)
         print(AI_input["content"])
 
         messages.append(AI_input)
+
+        print(messages)
 
         shell.send(AI_input)
 
