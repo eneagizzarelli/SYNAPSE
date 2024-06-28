94e65af83a04861c04ca556204d0a8acb7c9cd18
eneagizzarelli <s314887@studenti.polito.it>
2024-06-28 11:13:15 +0200

    fix: minor fixes

--- use_cases/AI_vs_SYNAPSE.py
+++ use_cases/AI_vs_SYNAPSE.py
@@ -1,11 +1,12 @@
 import sys
 import subprocess
+import select
 
 sys.path.append("/home/enea/SYNAPSE/src")
 from ai_requests import generate_response
 
 process = subprocess.Popen(
-    ['python3', 'SYNAPSE.py'],
+    ['python3', '../src/SYNAPSE.py'],
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
@@ -13,10 +14,18 @@ process = subprocess.Popen(
 )
 
 while True:
-    output = process.stdout.readline()
-    if output:
-        print(f"Script output: {output.strip()}")
-    else:
+    ready, _, _ = select.select([process.stdout], [], [], 1.0)
+
+    if ready:
+        output = process.stdout.readline()
+        if output:
+            print(f"Script output: {output.strip()}")
+        else:
+            print("suca1")
+            break
+
+    if process.poll() is not None:
+        print("suca2")
         break
 
 process.stdin.close()
