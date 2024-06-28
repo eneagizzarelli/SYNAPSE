dbc39dfa4c1664f0f590d8e7198d16937e785cef
eneagizzarelli <s314887@studenti.polito.it>
2024-06-28 11:41:19 +0200

    feat: implementing AI_vs_SYNAPSE use case

--- use_cases/AI_vs_SYNAPSE.py
+++ use_cases/AI_vs_SYNAPSE.py
@@ -1,4 +1,5 @@
 import paramiko
+import time
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -10,10 +11,12 @@ try:
 
     initial_output = shell.recv(1024).decode()
 
-    print(initial_output)
+    print(initial_output, end='')
 
-    shell.send('ls -l\n')
-    
+    shell.send('cd ..\n')
+
+    time.sleep(2)
+
     command_output = shell.recv(1024).decode()
     print(command_output)
 finally:
