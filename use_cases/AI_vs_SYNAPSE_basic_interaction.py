06079d3308a8829ff584f3a3bac4a0c797aad78e
eneagizzarelli <s314887@studenti.polito.it>
2024-06-28 15:55:31 +0200

    fix: minor fixes

--- use_cases/AI_vs_SYNAPSE_basic_interaction.py
+++ use_cases/AI_vs_SYNAPSE_basic_interaction.py
@@ -5,38 +5,41 @@ import paramiko
 sys.path.append("/home/enea/SYNAPSE/src")
 from ai_requests import generate_response
 
+# create a new SSH client
 client = paramiko.SSHClient()
+# set policy to auto add the host key
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+# minimal prompt definition to instruct the AI
 messages = [{"role": 'system', "content": "You are a Linux user interacting with a Linux OS terminal. " +
                                         "You can use not only the terminal, but also a MySQL service where, other than the root, user 'enea' with password 'password' exists. " +
                                         "Issue some commands to interact with the OS file system and the MySQL service. " + 
                                         "Generate just the command you want to execute, nothing else. \n"}]
 
+# connect to the SSH server using the provided credentials and start an interactive shell
 client.connect('localhost', 22, 'enea', 'password')
 shell = client.invoke_shell()
 
 try:
     while True:
-        if shell.recv_ready():
-            SYNAPSE_output = shell.recv(1024).decode()
+        SYNAPSE_output = shell.recv(1024).decode()
 
-            if "will be reported" not in SYNAPSE_output:
-                last_command = messages[-1]["content"]
-                if SYNAPSE_output.startswith(last_command):
-                    SYNAPSE_output = SYNAPSE_output[len(last_command):]
-                print(SYNAPSE_output, end='')
+        if "will be reported" not in SYNAPSE_output:
+            last_command = messages[-1]["content"]
+            if SYNAPSE_output.startswith(last_command):
+                SYNAPSE_output = SYNAPSE_output[len(last_command):]
+            print(SYNAPSE_output, end='')
 
-                messages.append({"role": 'user', "content": SYNAPSE_output})
+            messages.append({"role": 'user', "content": SYNAPSE_output})
 
-                AI_input = generate_response(messages)
-                print(AI_input["content"], end='')
+            AI_input = generate_response(messages)
+            print(AI_input["content"], end='')
 
-                messages.append(AI_input)
+            messages.append(AI_input)
 
-                shell.send(AI_input["content"] + '\n')
+            shell.send(AI_input["content"] + '\n')
 
-                time.sleep(5)
+            time.sleep(5)
 except KeyboardInterrupt:
     print("\nScript interrupted by user.")
 except OSError as osError:
