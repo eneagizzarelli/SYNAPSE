8eaf9b3c0fe2f8c9e6324c7f82fdca4abe445ff2
eneagizzarelli <s314887@studenti.polito.it>
2024-07-11 17:36:04 +0200

    fix: modified sentence generation introducing few-shot prompting

--- SYNAPSE-to-MITRE/classification.py
+++ SYNAPSE-to-MITRE/classification.py
@@ -98,7 +98,24 @@ def get_sentence(classification_file, client_ip):
         classification_history = classification_history_file.read()
 
         # classification messages initialization
-        classification_messages = [{"role": "system", "content": "Given the following log of commands executed in a terminal by a user with the corresponding terminal outputs, you need to take in mind that it corresponds to an attack. You have to generate a brief sentence that describes the attack, without too much care about responses. The output will be mapped to the MITRE ATT&CK database. Try to use words that help the automatic mapping. \n\n"}]
+        classification_messages = [{"role": "system", "content": "Given the following log of commands executed in a terminal by a user, you need to take in mind that it corresponds to an attack. " +
+                                    "You have to generate a brief sentence that describes the attack. " +
+                                    "Be aware that your summary will be mapped to the MITRE ATT&CK database using a specific ML model trained on a specific dataset. " +
+                                    "Try to use words in a way that the automatic mapping is accurate. " +
+                                    "The dataset is composed of the following fields: 'technique label', 'sub-technique label', 'technique name', 'sentence'." +
+                                    "The mapping will be done using the 'sentence' field, passing to the model the summary you generated. " +
+                                    "Here there are few examples from the dataset to let you understand how sentences should be structured: \n" +
+                                    "1. 'Adversaries may destroy data and files on specific systems or in large numbers on a network to interrupt availability to systems, services, and network resources.'\n" +
+                                    "2. 'Adversaries may delete or modify artifacts generated within systems to remove evidence of their presence or hinder defenses.'\n" +
+                                    "3. 'Adversaries may abuse command and script interpreters to execute commands, scripts, or binaries.'\n" +
+                                    "4. 'Adversaries may insert, delete, or manipulate data at rest in order to manipulate external outcomes or hide activity.'\n" +
+                                    "5. 'Adversaries may perform Endpoint Denial of Service  attacks to degrade or block the availability of services to users'\n" +
+                                    "6. 'Adversaries may attempt to make an executable or file difficult to discover or analyze by encrypting, encoding, or otherwise obfuscating its contents on the system or in transit.'\n" +
+                                    "7. 'Adversaries may exploit software vulnerabilities in an attempt to elevate privileges.'\n" +
+                                    "Be aware: you MUST be careful about each word you use. " +
+                                    "The model will output a mapping based on the words you produce, so try to avoid using misleading words that can bring the model to output the wrong result. " +
+                                    "For example, in case of a fork bomb attack, if you say that it can be used in a denial of service, the model will map it to the 'Endpoint Denial of Service' MITRE attack, which is not the best way to consider it, so it is wrong. " +
+                                    "Apply this reasoning to all your responses.\n\n"}]
 
         # append classification history to classification messages
         classification_messages.append({"role": "user", "content": classification_history})
